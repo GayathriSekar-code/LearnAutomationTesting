f605b9a2c8a2ed8f1dc08524804914139b89131a
GayathriSekar-code <gayathrisekar_88@yahoo.com>
2025-12-13 15:22:32 +0530

    Add files via upload

---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+import week1.day2.LearnMethods;
+
+public class FirstProgram {
+	/**
+	 * *shortcuts:
+	 * 1) main method: type main and do ctrl+space(entry point for the execution of the program)
+	 * 2) print statement: type syso and do ctrl+space
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone! Welcome to TestLeaf for the Selenium Course");
+		System.out.println("Happy Learning");
+	
+	}
+}
